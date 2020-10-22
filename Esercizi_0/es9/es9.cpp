f83191551e4e2b6d36296aa4b4156e5a6ae7272a
Perabola <nicolaperatoner@gmail.com>
2020-10-22 21:16:06 +0200

    robe varie

--- Esercizi_0/es9/es9.cpp
+++ Esercizi_0/es9/es9.cpp
@@ -3,15 +3,16 @@ using namespace std;
 int main() {
     cout<<"LETTERE MAIUSCOLE:\n";
     for(int i=65;i<=90;++i){
-        cout<<(char) i<<endl;
+        cout<<"CHAR:  "<<(char) i<<"  ASCII:  "<<i<<endl;
+        
     }
     cout<<"LETTERE MINUSCOLE:\n";
     for(int e=97; e<=122 ; ++e ) {
-        cout<<(char) e<<endl;
+        cout<<"CHAR:  "<<(char) e<<"  ASCII:  "<<e<<endl;
     }
     cout<<"NUMERI DAL 0 AL 9:\n";
     for(int f=48; f<=57 ; ++f ) {
-        cout<<(char) f<<endl;
+        cout<<"CHAR:  "<<(char) f<<"  ASCII:  "<<f<<endl;
     }
     return 0;
 }
