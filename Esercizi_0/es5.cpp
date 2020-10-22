d2487be84942f1db6dff547f10f02de3525d89cf
Perabola <nicolaperatoner@gmail.com>
2020-10-22 14:13:49 +0200

    Modifica es5.cpp

--- Esercizi_0/es5.cpp
+++ Esercizi_0/es5.cpp
@@ -8,37 +8,33 @@ int main() {
     cin>>y;
     cout<<"inserire terza cifra\n";
     cin>>z;
-    int m; // minore;
-    int M; // MAGGIORE;
     int S; // Somma;
     S=x+y+z;
     int Med; // Media;
     Med=S/3;
     int P; // Prodotto;
     P=x*y*z;
-    if(x>y & x>z) {   // x è il piu grande;
-        M=x;
-    }
-    if(x<y & x<z) {  // x è il piu piccolo;
-        m=x;
-    }
-    if(y>x & y>z) {  // y è il piu grande;
-        M=y;
-    }
-    if(y<x & y<z) {  // y è il piu piccolo;
-        m=y;
-    }
-    if(z>y & z>x) {  // z è il piu grande;
-        M=z;
-    }
-    if(z<y & z<x) {  // z è il piu piccolo;
-        m=z;
-    }
     cout<<"SOMMA....."<<S<<endl;
     cout<<"MEDIA....."<<Med<<endl;
     cout<<"PRODOTTO.."<<P<<endl;
-    cout<<M<<"  E' IL PIU GRANDE DI TUTTI\n";
-    cout<<m<<"  E' IL PIU PICCOLO DI TUTTI\n";
+    if(x>y&&x>z){   // x è il piu grande;
+                cout<<x<<"  E' IL PIU GRANDE DI TUTTI\n";
+    }
+    if(x<y&&x<z) {  // x è il piu piccolo;
+                cout<<x<<"  E' IL PIU PICCOLO DI TUTTI\n"; 
+    }
+    if(y>x&&y>z) {  // y è il piu grande;
+                cout<<y<<"  E' IL PIU GRANDE DI TUTTI\n";
+    }
+    if(y<x&&y<z) {  // y è il piu piccolo;
+                cout<<y<<"  E' IL PIU PICCOLO DI TUTTI\n";
+    }
+    if(z>y&&z>x) {  // z è il piu grande;
+                cout<<z<<"  E' IL PIU GRANDE DI TUTTI\n";
+    }
+    if(z<y&&z<x) {  // z è il piu piccolo;
+                cout<<z<<"  E' IL PIU PICCOLO DI TUTTI\n";
+    }
     return 0;
 
 }
