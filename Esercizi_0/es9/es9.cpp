b9293465b9972cb80adb251ebc0445514a8ec213
Perabola <nicolaperatoner@gmail.com>
2020-10-24 11:25:04 +0200

    modifica es9

--- Esercizi_0/es9/es9.cpp
+++ Esercizi_0/es9/es9.cpp
@@ -1,16 +1,22 @@
 using namespace std;
 #include <iostream>
 int main() {
-    cout<<"LETTERE MAIUSCOLE:\n";
+    cout<<"------------------\n"
+        <<"LETTERE MAIUSCOLE:\n"
+        <<"------------------\n";
     for(int i=65;i<=90;++i){
         cout<<"CHAR:  "<<(char) i<<"  ASCII:  "<<i<<endl;
         
     }
-    cout<<"LETTERE MINUSCOLE:\n";
+    cout<<"------------------\n"
+        <<"LETTERE MINUSCOLE:\n"
+        <<"------------------\n";
     for(int e=97; e<=122 ; ++e ) {
         cout<<"CHAR:  "<<(char) e<<"  ASCII:  "<<e<<endl;
     }
-    cout<<"NUMERI DAL 0 AL 9:\n";
+    cout<<"------------------\n"
+        <<"NUMERI DAL 0 AL 9:\n"
+        <<"------------------\n";
     for(int f=48; f<=57 ; ++f ) {
         cout<<"CHAR:  "<<(char) f<<"  ASCII:  "<<f<<endl;
     }
