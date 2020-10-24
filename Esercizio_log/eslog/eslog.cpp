51d2232a39fc6c573f69df87f2cd8b4ba380e835
Perabola <nicolaperatoner@gmail.com>
2020-10-24 11:25:55 +0200

    modifica es9

---
+++ Esercizio_log/eslog/eslog.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int main () {
+    for(int i=1; i<=34; ++i) {
+        cout<<i<<"....."<<log(i)/log(2.0)<<endl;
+    }
+    return 0;
+}
